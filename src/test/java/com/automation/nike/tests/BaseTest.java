752eac698ef1fdde26e2483e9a3916c02c63821f
Negus Collis <neguscollis@Neguss-MBP.lan>
2024-11-09 11:47:45 -0500

    First Push

---
+++ src/test/java/com/automation/nike/tests/BaseTest.java
@@ -0,0 +1,30 @@
+package com.automation.nike.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public abstract class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver" , "/Users/neguscollis/Desktop/Coding/Nike/src/test/resources/driver/chromedriver");
+        driver.get("https://www.nike.com");
+
+
+
+    }
+
+
+    @AfterMethod
+    public void cleanUp(){
+        driver.quit();
+    }
+
+
+}
---
+++ src/test/java/com/automation/nike/tests/SearchProductsTest.java
@@ -0,0 +1,16 @@
+package com.automation.nike.tests;
+
+import org.testng.annotations.Test;
+
+public class SearchProductsTest extends BaseTest{
+
+
+
+    @Test
+    public void test(){
+
+    }
+
+
+
+}
